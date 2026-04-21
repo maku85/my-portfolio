47652582afac7504331eee145dbe6aa44b8ef764
Maku <makuma85@gmail.com>
2026-04-21 20:39:03 +0200

    fix: clean portfolio

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,17 +1,12 @@
-import DontClickRed from "../games/DontClickRed/DontClickRed";
 import AboutCard from "./AboutCard";
 import DevNewsCard from "./DevNewsCard";
 import ExperiencesCard from "./ExperiencesCard";
-import GamesCard from "./GamesCard";
 import GitHubIssuesCard from "./GitHubIssuesCard";
 import GithubStatsCard from "./GithubStatsCard";
 import LibrariesCard from "./LibrariesCard";
-import PinballCard from "./PinballCard";
 import ProjectsCard from "./ProjectsCard";
 import SkillsCard from "./SkillsCard";
 import SocialsCard from "./SocialsCard";
-import SpotifyCard from "./SpotifyCard";
-import StoriesCard from "./StoriesCard";
 import WelcomeCard from "./WelcomeCard";
 import WIPCard from "./WIPCard";
 
@@ -27,9 +22,4 @@ export const initialCards = [
   GithubStatsCard,
   GitHubIssuesCard,
   DevNewsCard,
-  GamesCard,
-  StoriesCard,
-  SpotifyCard,
 ];
-
-export const gameCards = [DontClickRed, PinballCard];
